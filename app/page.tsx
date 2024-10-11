b7eef0e2067a4c63c2b16a309bf79de15e83cd51
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-11 10:43:46 +0530

    Updating the pic to text converter

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,38 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Tesseract from "tesseract.js";
 import Image from "next/image";
+import toast, { Toaster } from "react-hot-toast";
 
 const ImageToText = () => {
   const [image, setImage] = useState<string | null>(null);
   const [text, setText] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  useEffect(() => {
+    if (isLoading) {
+      toast.loading("Converting...");
+    } else {
+      toast.dismiss();
+    }
+  }, [isLoading]);
+
+  useEffect(() => {
+    toast.dismiss();
+    if (text) {
+      toast.success("Text extracted successfully");
+    }
+  });
+
+  const handleButtonClick = () => {
+    const fileInput = document.getElementById("file");
+    fileInput?.click();
+  };
+
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
+    console.log(file?.name);
     if (file) {
       setImage(URL.createObjectURL(file));
     }
@@ -32,31 +54,71 @@ const ImageToText = () => {
         });
     } else {
       setIsLoading(false);
-      console.error("No image selected");
+      toast.error("No image selected");
     }
   };
 
   return (
-    <div className="container">
-      <h1>Image to Text Converter</h1>
+    <div className="container flex flex-col gap-10 items-center p-4">
+      <Toaster />
+      <h1 className="font-bold text-4xl mt-10 tracking-wide leading-6">
+        Pic2Text
+      </h1>
 
-      <input type="file" accept="image/*" onChange={handleImageUpload} />
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        onClick={() => setText("")}
+        className="hidden"
+        id="file"
+      />
 
       {image && (
-        <div>
-          <Image src={image} alt="uploaded" width={100} height={100} />
-          <button onClick={handleConvert} disabled={isLoading}>
-            {isLoading ? "Converting..." : "Convert to Text"}
-          </button>
+        <div className="flex flex-col gap-4 items-center">
+          <Image
+            src={image}
+            alt="uploaded"
+            width={300}
+            height={300}
+            className="rounded-md"
+          />
         </div>
       )}
 
-      {text && (
-        <div>
-          <h3>Extracted Text:</h3>
-          <p>{text}</p>
-        </div>
-      )}
+      <div className="flex gap-4 items-center">
+        <button
+          onClick={handleButtonClick}
+          className="p-[6px] bg-black text-white flex gap-2 items-center px-4 rounded-full font-bold border-2 border-black"
+        >
+          <Image
+            src="/upload.svg"
+            alt="upload"
+            width={25}
+            height={25}
+            className="fill-white"
+          />
+          <span>{image ? "Change Image" : "Upload Image"}</span>
+        </button>
+        {image && (
+          <button
+            onClick={handleConvert}
+            disabled={isLoading}
+            className="p-2 bg-black text-white flex gap-2 items-center px-4 rounded-full font-bold"
+          >
+            <Image
+              src="/convert.svg"
+              alt="upload"
+              width={22}
+              height={22}
+              className="fill-white"
+            />
+            <span>{isLoading ? "Converting..." : "Convert to Text"}</span>
+          </button>
+        )}
+      </div>
+
+      {text && <code>{text}</code>}
     </div>
   );
 };
